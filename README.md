98ba0fee1e1ca5d8aeb5fa3243cbad241c681fbb
LCLP <lukas@lclpnet.work>
2022-04-24 18:25:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# lclp-app-builder
+An application builder for LCLPLauncher apps.
+
+## Project Setup
+
+```sh
+yarn
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+yarn dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+yarn build
+```
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {RouterView} from 'vue-router'
+import '@/assets/base.scss';</script>
+
+<template>
+<!--  <header>-->
+<!--    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
+
+<!--    <div class="wrapper">-->
+<!--      <nav>-->
+<!--        <RouterLink to="/">Home</RouterLink>-->
+<!--      </nav>-->
+<!--    </div>-->
+<!--  </header>-->
+
+  <RouterView />
+</template>
